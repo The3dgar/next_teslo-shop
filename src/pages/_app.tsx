3044568698d97a19c832fb043ff59524cc63edf8
edgarolivar16 <edgarolivar16@gmail.com>
2023-05-03 22:15:24 -0400

    set theme config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,13 @@
 import '@/styles/globals.css';
-import { ThemeProvider } from '@mui/material';
+import { ThemeProvider, CssBaseline } from '@mui/material';
 import type { AppProps } from 'next/app';
+
+import { lightTheme } from '@/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={}>
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline/>
       <Component {...pageProps} />
     </ThemeProvider>
   );
